c2423291be6fd62b6667130c52d43f5d0a66f982
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-11-14 20:35:44 +0530

    Initial Setup of App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
