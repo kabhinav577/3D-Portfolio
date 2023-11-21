87de0fd49f0a6b534e3c3b1b4b17e2e1cdc2b83f
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-11-21 08:42:44 +0530

    feat: Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { About, Contact, Projects, Home } from './pages';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-      </h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
