2a72c586cc9a86bb0310bcb13cd6bc19b09b64b4
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-11-26 18:20:10 +0530

    Displaying Educational Data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,14 +4,14 @@ import {
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-import { skills, experiences } from '../constants';
+import { skills, experiences, educations } from '../constants';
 import CTA from '../components/CTA';
 
 const About = () => {
   return (
     <section className="max-container">
       <h1 className="head-text">
-        Hello, I'm{' '}
+        Hello, I&apos;m{' '}
         <span className="blue-gradient_text font-semibold drop-shadow">
           Krishnakant Singh
         </span>
@@ -24,12 +24,12 @@ const About = () => {
         </p>
       </div>
 
+      {/* Skills Section  */}
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
-
         <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill) => (
-            <div className="block-container w-20 h-20">
+          {skills.map((skill, i) => (
+            <div className="block-container w-20 h-20" key={i}>
               <div className="btn-back rounded-xl" />
               <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
@@ -43,13 +43,14 @@ const About = () => {
         </div>
       </div>
 
+      {/* Work Experiences Section  */}
       <div className="py-16">
         <h3 className="subhead-text">Work Experience</h3>
         <div className="mt-5 flex flex-col gap-3 text-slate-500">
           <p>
-            I'he worked on many projects when i was learning at Coding Ninjas,
-            leveling up my skills and teaming up with smart people. Here's the
-            rundown:
+            I&apos;ve worked on many projects when i was learning at Coding
+            Ninjas, leveling up my skills and teaming up with smart people.
+            Here&apos;s the rundown:
           </p>
         </div>
 
@@ -102,6 +103,69 @@ const About = () => {
           </VerticalTimeline>
         </div>
       </div>
+
+      {/* Education Sections  */}
+      <div className="py-16">
+        <h3 className="subhead-text">Education</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            My academic achievements reflect a commitment to excellence, and I
+            am eager to leverage this knowledge in a professional setting to
+            contribute to innovative solutions and technology-driven
+            advancements.
+          </p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {educations.map((education) => (
+              <VerticalTimelineElement
+                key={education.name}
+                date={education.date}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img
+                      src={education.icon}
+                      alt={education.academic_name}
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                }
+                iconStyle={{
+                  background: education.iconBg,
+                }}
+                contentStyle={{
+                  borderBottom: '8px',
+                  borderStyle: 'solid',
+                  borderBottomColor: education.iconBg,
+                  boxShadow: 'none',
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {education.name}
+                  </h3>
+                  <p className="text-black-500 font-medium font-base">
+                    {education.academic_name}
+                  </p>
+                </div>
+
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {education.points.map((point, i) => (
+                    <li
+                      key={i}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
       <hr className="border-slate-200" />
       <CTA />
     </section>
